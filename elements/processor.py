f7d139acd3d1166928346008d076dc8f14b494fc
pmoros <pmoros@outlook.com>
2022-10-08 18:15:37 -0500

    feat: from image to segmented matrix

---
+++ elements/processor.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def from_image_to_segmented_matrix(img):
+    # crop image to remove the borders
+    height, width, channels = img.shape
+    cropped_img = img[int(0.15*height):, int(0.01*width)
+                          :width - int(0.01*width)]
+
+    image_array = np.array(cropped_img)
+    y_height = image_array.shape[0]
+    x_width = image_array.shape[1]
+    xs = x_width//10  # division lines for the picture
+    ys = y_height//13
+    # now slice up the image (in a shape that works well with subplots)
+    splits = []
+    for x, i in enumerate(range(0, y_height, xs)):
+        aux = []
+        for y, j in enumerate(range(0, x_width, ys)):
+            aux.append(image_array[i:i+xs, j:j+ys])
+        splits.append(aux)
+    return splits
---
+++ tests/unit/test_processor.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from unittest import TestCase
+from unittest.mock import mock_open, patch
+import cv2
+from elements import processor
+
+
+class TestProcessor(TestCase):
+
+    def test_from_image_to_symbols_matrix(self):
+        pass
+
+    def test_from_image_to_segmented_matrix(self):
+        base_path = "./resources/templates/"
+        RAW_IMAGE_URI = base_path + "levels/01.png"
+        img = cv2.imread(RAW_IMAGE_URI)
+        segmented_matrix = processor.from_image_to_segmented_matrix(img)
+        self.assertEqual(
+            (len(segmented_matrix), len(segmented_matrix[0])), (14, 11))
+
+    def test_get_map_image_matrix(self):
+        pass
